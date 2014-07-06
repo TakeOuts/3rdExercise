7ebefad5c3abfeb428107bfec2236093832d35b0
Ivan Petrov <e-rss@ya.ru>
2014-07-06 13:26:15 +0400

    Second commit

--- 3rd.rb
+++ 3rd.rb
@@ -6,6 +6,15 @@ require 'net/http'
 # Скрипт должен выкладывать входящую строку на этот сервис, на выходе 
 # скрипт должен выдавать соответствующую ссылку.
 
+# Проверка на существование аргументов, которые, по условиям задачи, не должны влиять на работу программы
+if ARGV.count != 0
+  print "Введенные Вами аргументы:"
+  ARGV.each do|a|
+    print " #{a},"
+  end
+  puts " не используются в данной программе"
+end
+
 # Адрес API сайта Pastebin
 uri = URI('http://pastebin.com/api/api_post.php')
 
@@ -20,17 +29,14 @@ api_paste_private = '0' # 0=public 1=unlisted 2=private
 loop do
   puts "Введите название документа: "
   # Переменная @api_paste_name объявлена как переменная экземпляра
-  @api_paste_name = gets.chomp
-  
-  puts "Введите содержимое документа: "
-  @api_paste_code = gets.chomp
+  @api_paste_name = STDIN.gets.chomp
   
   # Если введенный текст не является пустой строкой и не превышает лимиты, то выходим из цикла
-  unless @api_paste_name.empty? || @api_paste_code.empty?
-    if @api_paste_name.length < 100 && @api_paste_code.length < 500
+  unless @api_paste_name.empty?
+    if @api_paste_name.length < 100
       break
     else
-      puts "Превышен лимит длины названия или документа"
+      puts "Превышен лимит длины названия"
     end    
   # В противном случае выводим сообщение об ошибке
   else
@@ -38,6 +44,23 @@ loop do
   end
 end
 
+# Заполнение содержимого документа
+loop do
+  puts "Введите содержимое документа: "
+  @api_paste_code = STDIN.gets.chomp
+  
+  # Если введенный текст не является пустой строкой и не превышает лимиты, то выходим из цикла
+  unless @api_paste_code.empty?
+    if @api_paste_code.length < 500
+      break
+    else
+      puts "Превышен лимит длины документа"
+    end    
+  # В противном случае выводим сообщение об ошибке
+  else
+    puts "Введена пустая строка"
+  end
+end
 
 # Используется механизм исключений для обработки ошибок соединения.
 begin
